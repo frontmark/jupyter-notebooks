a2eca1e68d74430e9b5e61c32fab0c5757d7d151
Stefan <54994054+FuggeWolf@users.noreply.github.com>
2020-07-17 10:48:23 +0200

    admin

--- pyriva/instruments/specifications.py
+++ pyriva/instruments/specifications.py
@@ -59,8 +59,8 @@ def ZeroBondSpecification(obj_id: str, curr: str, issue_date: _datetime, expiry:
         sec_level (str, optional): Zero bond's securitization level. Defaults to 'NONE'.
 
     Returns:
-        BondSpecification: The general bond specification for pyvacon including all relevant parameters for the
-        zero bond specification based on the function's arguments.
+        BondSpecification: The general bond specification for pyvacon including all relevant parameters for
+        the zero bond specification based on the function's arguments.
     """
     return BondSpecification(obj_id, issuer, sec_level, curr, expiry, issue_date, notional, 'ACT365FIXED',
                              [], [], '', [], [])
@@ -82,48 +82,84 @@ def FixedRateBondSpecification(obj_id: str, curr: str, issue_date: _datetime, ex
         coupon_dates (_List[_datetime]): List of coupon payment dates.
         coupons (_List[float]): List of annualized rates of each coupon.
         notional (float, optional): Fixed rate bond's face value. Defaults to 100.0.
-        day_count (_datetime, optional): Day count convention method for coupon period length calculation.                               'ACT365FIXED'.
-                    Defaults to 'ACT365FIXED'.
+        day_count (_datetime, optional): Day count convention method for coupon period length calculation.
+        Defaults to 'ACT365FIXED'.
         issuer (str, optional): Issuer of the fixed rate bond. Defaults to 'dummy_issuer'.
         sec_level (str, optional): Fixed rate bond's securitization level. Defaults to 'NONE'.
 
     Returns:
-        BondSpecification: The general bond specification for pyvacon including all relevant parameters for the
-        fixed rate bond specification based on the function's arguments.
+        BondSpecification: The general bond specification for pyvacon including all relevant parameters for
+        the fixed rate bond specification based on the function's arguments.
     """
     return BondSpecification(obj_id, issuer, sec_level, curr, expiry, issue_date, notional, day_count,
                              coupon_dates, coupons, '', [], [])
 
 
-def FloatingRateBondSpecification(obj_id: str, curr: str, issue_date: _datetime, expiry: _datetime,
+def FloatingRateNoteSpecification(obj_id: str, curr: str, issue_date: _datetime, expiry: _datetime,
                                   coupon_periods: _List[_datetime], spreads: _List[float],
                                   reference_index: str = '', notional: float = 100.0,
                                   day_count: str = 'ACT365FIXED', issuer: str = 'dummy_issuer',
                                   sec_level: str = 'NONE') -> BondSpecification:
-    """ Specifies floating rate bond by minimal set of required information. The specification feeds into
+    """ Specifies floating rate note by minimal set of required information. The specification feeds into
     pyvacon.analytics' general BondSpecification. A more extensive floating rate bond specification also
     allows for the definition of issuer and securitization level.
 
     Args:
-        obj_id (str): Unique floating rate bond identifier.
-        curr (str): Floating rate bond's currency (ISO 4217).
-        issue_date (_datetime): Floating rate bond's issue date.
-        expiry (_datetime): Floating rate bond's maturity date.
+        obj_id (str): Unique floating rate note identifier.
+        curr (str): Floating rate note's currency (ISO 4217).
+        issue_date (_datetime): Floating rate note's issue date.
+        expiry (_datetime): Floating rate note's maturity date.
         coupon_periods (_List[_datetime]: Accrual periods (start and end dates) for floating rate coupons.
         spreads (_List[_datetime]): Add-on on top of floating rate determined by fixing of reference index.
         reference_index (str, optional): Underlying of floating rate coupon rates. Defaults to ''.
-        notional (float, optional): Floating rate bond's face value. Defaults to 100.0.
+        notional (float, optional): Floating rate note's face value. Defaults to 100.0.
         day_count (_datetime, optional): Day count convention method for coupon period length calculation.
-                    Defaults to 'ACT365FIXED'.
-        issuer (str, optional): Issuer of the floating rate bond. Defaults to 'dummy_issuer'.
-        sec_level (str, optional): Fixed floating bond's securitization level. Defaults to 'NONE'.
+        Defaults to 'ACT365FIXED'.
+        issuer (str, optional): Issuer of the floating rate note. Defaults to 'dummy_issuer'.
+        sec_level (str, optional): Fixed floating note's securitization level. Defaults to 'NONE'.
 
     Returns:
-        BondSpecification: The general bond specification for pyvacon including all relevant parameters for the
-        floating rate bond specification based on the function's arguments.
+        BondSpecification: The general bond specification for pyvacon including all relevant parameters for
+        the floating rate bond specification based on the function's arguments.
     """
     return BondSpecification(obj_id, issuer, sec_level, curr, expiry, issue_date, notional, day_count,
                              [], [], reference_index, coupon_periods, spreads)
+
+
+def GeneralBondSpecification(obj_id: str, curr: str, issue_date: _datetime, expiry: _datetime,
+                             coupon_dates: _List[_datetime], coupons: _List[float],
+                             coupon_periods: _List[_datetime], spreads: _List[float],
+                             reference_index: str = '', notional: float = 100.0,
+                             day_count: str = 'ACT365FIXED', issuer: str = 'dummy_issuer',
+                             sec_level: str = 'NONE') -> BondSpecification:
+    """ Specifies bonds with both fixed and floating rate coupons by minimal set of required information. The
+     specification feeds into pyvacon.analytics' general BondSpecification. A more extensive specification
+     also allows for the definition of issuer and securitization level.
+
+
+    Args:
+        obj_id (str): Unique bond note identifier.
+        curr (str): Bond's currency (ISO 4217).
+        issue_date (_datetime): Bond's issue date.
+        expiry (_datetime): Bond's maturity date.
+        coupon_dates (_List[_datetime]): List of fixed coupon payment dates.
+        coupons (_List[float]): List of annualized rates of each fixed coupon.
+        coupon_periods (_List[_datetime]: Accrual periods (start and end dates) for floating rate coupons.
+        spreads (_List[_datetime]): Add-on on top of floating rate determined by fixing of reference index.
+        reference_index (str, optional): Underlying of floating rate coupon rates. Defaults to ''.
+        notional (float, optional): Bonds's face value. Defaults to 100.0.
+        day_count (_datetime, optional): Day count convention method for coupon period length calculation.
+        Defaults to 'ACT365FIXED'.
+        issuer (str, optional): Issuer of the bond. Defaults to 'dummy_issuer'.
+        sec_level (str, optional): Bond's securitization level. Defaults to 'NONE'.
+
+    Returns:
+        BondSpecification: The general bond specification for pyvacon including all relevant parameters for
+        the specificaiton of bonds with both fixed and floating rate coupons based on the function's
+        arguments.
+    """
+    return BondSpecification(obj_id, issuer, sec_level, curr, expiry, issue_date, notional, day_count,
+                             coupon_dates, coupons, reference_index, coupon_periods, spreads)
 
 
 InflationLinkedBondSpecification = _add_converter(_analytics.InflationLinkedBondSpecification)
